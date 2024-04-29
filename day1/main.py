d6da020335c88db01b58e420c54b9a08950fbfcd
hiren007saliya <saliyahiren@gmail.com>
2024-04-29 15:27:30 +0200

    first commit

---
+++ day1/main.py
@@ -0,0 +1,29 @@
+# print("hello world")
+# print("i am hiren saliya")
+# print('print("my last name is saliya")')
+# print("hiren" + " "+"saliya")
+
+# print("day 1 - sring manipulation")
+# print('sting concatination is done with the " + "sign.')
+# print('e.g. print("hello" + "world")')
+# print("new line can be created with a backslash and n.")
+# print("hello "+ input("what is your name?"))
+
+# name =input("what is your name?\n")
+# print(len(name))
+
+# a=input("a:")
+# b=input("b:")
+
+# c=a
+# a=b
+# b=c
+
+# print("a="+a)
+# print("b="+b)
+#
+print("welcome to the band name generator.")
+city=input("what is name of the city you grew up in?\n")
+pet = input("what is the name of pet\n")
+print("you name should be "+city+" "+pet)
+
